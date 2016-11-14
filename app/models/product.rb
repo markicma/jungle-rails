71bab77e2b6bce93946b1dfb19d762fbbac85668
Matthew Markic <matt.markic@hotmail.com>
2016-11-14 17:19:44 +0000

    Created model spec for products to test validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,3 +13,14 @@ class Product < ActiveRecord::Base
   validates :category, presence: true
 
 end
+
+# create_table "products", force: :cascade do |t|
+#   t.string   "name"
+#   t.text     "description"
+#   t.string   "image"
+#   t.integer  "price_cents"
+#   t.integer  "quantity"
+#   t.datetime "created_at",  null: false
+#   t.datetime "updated_at",  null: false
+#   t.integer  "category_id"
+# end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'will contain an id when saved' do
+      @category = Category.create(name: "Sports")
+      @product = Product.create(name: "Basketball Shoes", quantity: 50, price: 100.00, category_id: @category.id)
+
+      expect(@product.id).to be_present
+    end
+
+    it 'will require a price when created' do
+      @category = Category.create(name: "Sports")
+      @product = Product.create(name: nil, quantity: 50, price: 100.00, category_id: @category.id)
+
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'will require a price when created' do
+      @category = Category.create(name: "Sports")
+      @product = Product.create(name: "Basketball Shoes", quantity: 50, price: nil, category_id: @category.id)
+
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'will require a quantity when created' do
+      @category = Category.create(name: "Sports")
+      @product = Product.create(name: "Basketball Shoes", quantity: nil, price: 100.00, category_id: @category.id)
+
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'will require a category when created' do
+      @category = Category.create(name: "Sports")
+      @product = Product.create(name: "Basketball Shoes", quantity: 50, price: 100.00, category_id: nil)
+
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
+end
